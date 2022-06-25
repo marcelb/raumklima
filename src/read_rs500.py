051f42a0807f14af693c820aceea819097c99060
Marcel Bankmann <adsci81@gmail.com>
2022-06-25 23:09:14 +0100

    output json instead

--- src/read_rs500.py
+++ src/read_rs500.py
@@ -6,14 +6,18 @@ from rs500reader.reader import Rs500Reader
 def get_and_print():
     reader = Rs500Reader()
     data = reader.get_data()
-    print('--------------------------------')
-    print('Channel | Temperature | Humidity')
-    print('================================')
+    print('{')
+    print('"channels" : [')
     for i in range(1, 9, 1):
         chan_data = data.get_channel_data(i)
         if chan_data is not None:
-            print('{:7d} | {:8.1f} °C | {:6d} %'.format(i, chan_data.temperature, chan_data.humidity))
-    print('================================')
+            if i != 1:
+                print(", ")
+            print('{')
+            print('"id" : {:7d}, "temp" : {:8.1f}, "hum" : {:6d}'.format(i, chan_data.temperature, chan_data.humidity))
+            print('}')
+    print(']')
+    print('}')
 
 
 if __name__ == '__main__':
